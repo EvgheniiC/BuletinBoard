95fa36607ceb1f47cd56897fa88d869c6aba4ae4
Evghenii Costin <evghenii.costin@uni-oldenburg.de>
2020-09-16 11:57:07 +0200

    UserPepository was added

---
+++ src/main/java/com/evghenii/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.evghenii.repository;
+
+import com.evghenii.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByFirstName(String name);
+}
